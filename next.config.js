37c0a80ee05d453ef84568117814439bb74d7bd1
Jesse McLean <essejmclean@gmail.com>
2020-05-14 12:45:05 -0600

    Upgraded to next 9.4 and readjusted absolute import solution

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  webpack(config) {
-    config.resolve.alias["components"] = path.join(__dirname, "src/components");
-    config.resolve.alias["utils"] = path.join(__dirname, "src/utils");
-    return config;
-  },
-};
